ebea1dda33ac77657bee5281aacee8b47a487aaa
JustinM9six <jdm9six@gmail.com>
2019-12-11 23:24:10 -0500

    Added login

--- project0-ui/src/App.tsx
+++ project0-ui/src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Login } from './components/login/Login';
+import B99Nav from './components/navbar/Navbar';
 
 const App: React.FC = () => {
   return (
+    <Router>
     <div className='App'>
+      <nav>
+        <B99Nav />
+      </nav>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <Switch>
+          <Route path='/' component={Login} />
+        </Switch>
       </header>
     </div>
+    </Router>
   );
 };
 
--- project0-ui/src/components/login/Login.tsx
+++ project0-ui/src/components/login/Login.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Form, Row, Col, FormGroup, Label, Input, Button } from 'reactstrap';
 
 export class Login extends React.Component {
     render() {
         return (
             <div>
-                <p>This is where the login will go</p>
+                <Form>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="exampleUsername">Username</Label>
+                                <Input type="text" name="username" id="exampleUsername" placeholder="with a placeholder" />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="examplePassword">Password</Label>
+                                <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Button>Sign in</Button>
+                </Form>
             </div>
         );
     }
